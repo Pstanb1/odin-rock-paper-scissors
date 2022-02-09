aaf7563da84cf940626be2900a2da45f3f074b0d
Peter Stanbridge <pstanbridge@hotmail.co.uk>
2022-02-09 21:41:30 +0000

    Added scores and tried to begin building game()

--- script.js
+++ script.js
@@ -12,9 +12,15 @@ function computerPlay()
 
 var playerSelection = prompt('Enter Rock, Paper or Scissors!');
 var computerSelection = computerPlay();
+var playerScore = 0
+var computerScore = 0
+
+
+
 //function playRound() that compares playerSelection and computerSelection and returns result
 
-function playRound() {
+function playRound() 
+{
     if (playerSelection.toLowerCase() === computerSelection) {
         return 'It\'s a draw!';
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection == 'scissors') {
@@ -30,8 +36,21 @@ function playRound() {
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection == 'rock') {
         return 'You lose! Rock beats scissors!';
     }
+} if (playRound().includes('win')) {
+    playerScore++;
+} else if (playRound().includes('lose')) {
+    computerScore++;
 }
-console.log(computerSelection);
-console.log(playRound());
 
 //function game() that uses playRound() to play a best of 5 game
+
+//function game(playerScore,computerScore) {
+//   while (playerScore<3 || computerScore<3) {
+//        playerSelection.playRound();
+//    }
+//}
+
+
+console.log(computerSelection);
+alert(playRound());
+alert(`Player ${playerScore} Computer ${computerScore}`);
